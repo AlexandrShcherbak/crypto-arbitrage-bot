0a947973713f57e880e4256d4498f974a747225e
Alex Scherbak <scherbakalexanders@gmail.com>
2026-02-18 19:52:26 +0300

    Update __init__.py

---
+++ api/__init__.py
@@ -0,0 +1,5 @@
+from .binance_api import get_binance_p2p
+from .bybit_api import get_bybit_p2p
+from .coincap_api import get_assets_prices
+
+__all__ = ["get_assets_prices", "get_binance_p2p", "get_bybit_p2p"]
